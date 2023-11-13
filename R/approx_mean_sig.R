7ef2a47be6da2307328320bfcf4710399d87da5e
babichmorrowc <babichmorrowc@gmail.com>
2023-11-13 16:17:46 +0000

    Function for computing AMS

---
+++ R/approx_mean_sig.R
@@ -0,0 +1,12 @@
+approx_mean_sig <- function(predictions, labels, weights, b_reg = 110) {
+  true_pos <- sum(weights[predictions == "s" & labels == "s"])
+  false_pos <- sum(weights[predictions == "s" & labels == "b"])
+  ams <- sqrt(2 * (true_pos + false_pos + b_reg) * log(1 + (true_pos / (false_pos + b_reg))) - true_pos)
+  return(ams)
+}
+
+# Test AMS if we predicted all background
+predictions <- rep("b", nrow(higgs_data))
+labels <- higgs_data$Label
+weights <- higgs_data$Weight
+approx_mean_sig(predictions, labels, weights) # give us 0
