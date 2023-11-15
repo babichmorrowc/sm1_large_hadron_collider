1f89f98f87b2506f8b1b12c1ff3a804ec6f9f727
babichmorrowc <babichmorrowc@gmail.com>
2023-11-15 10:39:18 +0000

    Run FDA: using -999, removing missing vars

--- R/clean_data.R
+++ R/clean_data.R
@@ -9,10 +9,24 @@ higgs_data_orig <- read_csv("./data/atlas-higgs-challenge-2014-v2.csv")
 skim(higgs_data_orig) # quick data summary
 
 # Clean data -------------------------------------------------------------------
-higgs_data <- higgs_data_orig %>% 
+# Dataframe with just the predictors and label variables
+higgs_vars <- higgs_data_orig %>% 
+  dplyr::select(starts_with(c("DER", "PRI", "Label")))
+
+# Dataframe replacing the -999 with NA
+higgs_data_na <- higgs_data_orig %>% 
   # Replace all of the -999 values with NA
   mutate(across(where(is.numeric), ~na_if(.x, -999)))
-
 # Skim of data with NA values
 # complete_rate shows variables with lots of missing data
-skim(higgs_data)
+skim(higgs_data_na)
+
+# Get the names of all columns with missing data
+missing_vars <- higgs_data_na %>% 
+  dplyr::select(where(~any(is.na(.)))) %>% 
+  names()
+
+# EDA --------------------------------------------------------------------------
+# Example histogram of a variable split by signal vs. background
+ggplot(higgs_data_na, aes(x = DER_lep_eta_centrality, fill = Label)) +
+  geom_histogram()
---
+++ R/fda.R
@@ -0,0 +1,42 @@
+# Perform Fisher Discriminant Analysis on the data
+source("R/clean_data.R")
+
+library(MASS) # package for lda
+library(caret) # ML package
+
+# Training / test split --------------------------------------------------------
+# Take 80% of the data for training, 20% for testing
+set.seed(999)
+index <- createDataPartition(higgs_vars$Label, p = 0.8, list = FALSE)
+fda_training <- higgs_vars[index,]
+fda_testing <- higgs_vars[-index,]
+
+# create dataframes without the missing variables
+fda_training_nomissing <- fda_training %>% 
+  dplyr::select(-all_of(missing_vars))
+fda_testing_nomissing <- fda_testing %>% 
+  dplyr::select(-all_of(missing_vars))
+
+# Run FDA ----------------------------------------------------------------------
+# using the original data with -999 values
+fda <- lda(Label ~ ., fda_training)
+# Getting:
+# Warning message:
+#   In lda.default(x, grouping, ...) : variables are collinear
+fda
+
+fda_pred <- predict(fda, fda_training)
+ldahist(data = fda_pred$x[,1], g = fda_training$Label) # not a lot of separation
+
+# using only variables without missing values
+fda_nomissing <- lda(Label ~ ., fda_training_nomissing)
+# Still getting:
+# Warning message:
+#   In lda.default(x, grouping, ...) : variables are collinear
+fda_nomissing
+
+fda_nomissing_pred <- predict(fda_nomissing, fda_training_nomissing)
+ldahist(data = fda_nomissing_pred$x[,1], g = fda_training_nomissing$Label) # still not a lot of separation
+
+
+
