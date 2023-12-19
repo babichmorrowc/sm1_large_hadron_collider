fe0202dc9278a2860d2b7773cc76a1fbc338ef85
babichmorrowc <babichmorrowc@gmail.com>
2023-12-19 21:26:55 +0000

    Try SVMs with all variables

--- R/svm.R
+++ R/svm.R
@@ -2,14 +2,28 @@ library(e1071) # SVM package
 
 # SVM --------------------------------------------------------------------------
 #### All variables ####
-# With a linear kernel
-svm_linear_all <- svm(Label ~ ., data = higgs_training, kernel = "linear")
+##### With a linear kernel #####
+# svm_linear_all <- svm(Label ~ ., data = higgs_training, kernel = "linear")
+# saveRDS(svm_linear_all, here("output/svm_linear_all.RDS")) # save object since it takes a long time to make
+svm_linear_all <- readRDS(here("output/svm_linear_all.RDS"))
 summary(svm_linear_all)
-fitted_svm_linear_all <- predict(svm_linear_all, higgs_testing, type = "response")
-fitted_svm_linear_all <- unlist(ifelse(fitted_svm_linear_all > 0.5, 1, 0))
-fitted_svm_linear_all_s_b <- ifelse(fitted_all == 1, "s", "b")
+# fitted_svm_linear_all <- predict(svm_linear_all, higgs_testing)
+# saveRDS(fitted_svm_linear_all, here("output/fitted_svm_linear_all.RDS")) # save object since it takes a long time to make
+fitted_svm_linear_all <- readRDS("output/fitted_svm_linear_all.RDS")
 
 # Calculate AMS
-approx_mean_sig(predictions = fitted_all_s_b,
-                labels = ifelse(higgs_testing$Label == 1, "s", "b"),
+approx_mean_sig(predictions = fitted_svm_linear_all,
+                labels = higgs_testing$Label,
                 weights = testing_weights)
+# 2.009864
+
+##### With a radial kernel #####
+svm_radial_all <- svm(Label ~ ., data = higgs_training, kernel = "radial")
+summary(svm_radial_all)
+fitted_svm_radial_all <- predict(svm_radial_all, higgs_testing)
+approx_mean_sig(predictions = fitted_svm_radial_all,
+                labels = higgs_testing$Label,
+                weights = testing_weights)
+# 2.743058
+
+
---
+++ R/training_testing.R
@@ -0,0 +1,35 @@
+library(caret) # ML package
+
+# Training vs test split -------------------------------------------------------
+set.seed(999)
+index <- createDataPartition(higgs_vars$Label, p = 0.05, list = FALSE)
+# Training data
+higgs_training <- higgs_vars[index,] %>% 
+  mutate(Label = as.factor(Label))
+  # mutate label to 0s and 1s
+  # mutate(Label = ifelse(Label == "s", 1, 0))
+# Dropping uniform and collinear
+higgs_training_drop_combo <- higgs_training %>% 
+  dplyr::select(-all_of(unif_vars),
+                -c(PRI_jet_leading_pt,
+                   PRI_jet_subleading_pt,
+                   PRI_lep_pt,
+                   PRI_tau_pt))
+
+# Testing data
+higgs_testing <- higgs_vars[-index,] %>% 
+  mutate(Label = as.factor(Label))
+  # mutate label to 0s and 1s
+  # mutate(Label = ifelse(Label == "s", 1, 0))
+# Dropping uniform and collinear
+higgs_testing_drop_combo <- higgs_testing %>% 
+  dplyr::select(-all_of(unif_vars),
+                -c(PRI_jet_leading_pt,
+                   PRI_jet_subleading_pt,
+                   PRI_lep_pt,
+                   PRI_tau_pt))
+# Calculate adjusted weights for testing data
+testing_weights <- adjust_weights(complete_data = higgs_data_orig,
+                                  subset_data = higgs_data_orig[-index,],
+                                  unadjusted_weight_col = "Weight",
+                                  label_col = "Label")
