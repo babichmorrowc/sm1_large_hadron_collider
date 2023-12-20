727cf88229f0a09d9195b708bcc1812179d9ac84
NonDescriptMaths <kieranmorris77@gmail.com>
2023-12-20 15:42:46 +0000

    Update Mutual_Information.R

--- R/Mutual_Information.R
+++ R/Mutual_Information.R
@@ -1,10 +1,18 @@
-source(here("R/variable_plots_and_MIcomputation.R"))
+library(here)
+library(ggplot2)
+library(infotheo)
+
+source(here("R/clean_data.R"))
+source(here("R/variable_plots.R"))
 
 KL_divergence <- function(X,Y){
   
 }
 
-Mutual_Information <- function(X,Y){
-  
-  
+Mutual_Information <- function(parameter){
+  XYdata <- gg_build(plots(parameter))$data[[1]]
+  print(XYdata)
 }
+
+V <- higgs_vars
+Mutual
