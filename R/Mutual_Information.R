e4219de28bb8db9d97ef288b9c88e0ad03edf876
NonDescriptMaths <kieranmorris77@gmail.com>
2023-12-22 22:16:36 +0000

    mutinfo updates
    annotation and making mutinfo actually output the thing

--- R/Mutual_Information.R
+++ R/Mutual_Information.R
@@ -8,17 +8,23 @@ source(here("R/variable_plots.R"))
 KL_divergence <- function(X,Y){
   
 }
-
+#|-----------------------------------------------|
+#|
 Mutual_Information <- function(parameter){
   XYdata <- ggplot_build(plots(parameter))$data[[1]]
+  #obtains the data for the variable given b and s from the variable_plots file
   Para_GivenB <- filter(XYdata, fill =="#F8766D")$y
   Para_GivenS <- filter(XYdata,fill != "#F8766D")$y
+  #use the colour of each plot to filter the densities into the b and s cases
+  #take the y value out, so we have a set of outputs for random variables
   ParaB <- discretize(Para_GivenB, nbins = 200)
   ParaS <- discretize(Para_GivenS,nbins = 200)
+  #discretize the data to make it compatible with the mutinfo function
   return(mutinformation(ParaS,ParaB))
 }
-
-names(higgs_vars)
-MIData <- data.frame(names = sapply(names(higgs_vars),Mutual_Information))
-
+#
+header <- names(higgs_vars)
+MIData <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
+MI_Data_Ordered <- MIData[order(MIData$data),]
+head(MI_Data_Ordered,n= 10)
 
--- R/variable_plots.R
+++ R/variable_plots.R
@@ -11,28 +11,35 @@ library(viridis)
 source(here("R/clean_data.R"))
 
 #|-------------------------------------------------|
-#function which produces a density plot for each variable, plotting the variable given b and s respectively
+#function which produces a density plot for each variable, with two different densities
+# which are the the variable given that it is labelled background and labelled as a signal respectively
 
 plots <- function(Parameter){
+  #function takes each variable as input
   pl <- ggplot(higgs_data_na, aes(x= get(Parameter), fill = Label)) +
+    #create ggplot with higgs_data_na splitting the data into two categories by using the Label (={b,s}) variable
     geom_density(alpha = 0.5) +
+    #choose opacity of plots
     labs(x = Parameter)
   return(pl)
 }
 
 #|-------------------------------------------------|
-#function which produces the joint distribution of the var|b and var|s respectively
+#function which produces the joint distribution of the var|b and var|s 
 plots_pairwise <- function(P1){
+  #takes in variable as an input
   mydiff <- function(data, diff){return(c(data, rep(NA, diff)))}
   P1_b <- as.vector(unlist(filter(higgs_data_na,Label == "b")[P1]))
   P1_s <- as.vector(unlist(filter(higgs_data_na, Label == "s")[P1]))
+  #filter the variable into its data points given that they are labelled signal and background
   difference = abs(length(P1_b)-length(P1_s))
   smaller_P1 <- min(length(P1_b),length(P1_s))
   if (smaller_P1 == length(P1_b)){
     P1_b <- mydiff(P1_b,difference)
   } else{
     P1_s <- mydiff(P1_s,difference)}
-  #processing variables for plotting^^^^
+  #the above fills in the shorter vector between P1_b and p1_s with na values
+  #this is because stat_bin_2d won't acceot different length vectors as input
   pl <- ggplot() +
     stat_bin_2d(bins = 70,aes(x=P1_b,y = P1_s))+
     theme_bw()
@@ -50,12 +57,12 @@ plots_product <- function(P1){
 
 #|-------------------------------------------------|
 #define which families of variables to plot
-cols_ToPlot <- c(colnames(higgs_data_na)[8:9],colnames(higgs_data_na)[29:30]) 
-cols_ToPlot2 <- c(colnames(higgs_data_na)[8:27],colnames(higgs_data_na)[29:30]) 
-cols_ToPlot3 <- c(colnames(higgs_data_orig)[8:27],colnames(higgs_data_orig)[29:30]) 
+cols_ToPlot <- c(colnames(higgs_data_na)[8:9],colnames(higgs_data_na)[29:30]) #random sample of histograms
+cols_ToPlot2 <- c(colnames(higgs_data_na)[1:30]) #all variables but with na values put in
+cols_ToPlot3 <- c(colnames(higgs_data_orig)[1:30]) #all variables but with -999 values put in
 #LIST OF RELEVANT PLOTS FOR THE RMD FILE
 #|----------------------------------------------------------|
-#SAMPLE PLOTS
+#SAMPLE PLOTS for variable analysis section
 #|--------------------------------------------|
 #|----------------------------|
 #sample 1D plots
