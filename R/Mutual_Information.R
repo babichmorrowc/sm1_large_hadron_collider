dc9b5274a1520dad6fd7fdae200ec97a38511915
NonDescriptMaths <kieranmorris77@gmail.com>
2023-12-26 17:18:08 +0000

    Update Mutual_Information.R

--- R/Mutual_Information.R
+++ R/Mutual_Information.R
@@ -22,9 +22,26 @@ Mutual_Information <- function(parameter){
   #discretize the data to make it compatible with the mutinfo function
   return(mutinformation(ParaS,ParaB))
 }
-#
+#|-----------------------------------------------|
+
+Mutual_Information2 <- function(parameter){
+  PGivenB <- filter(higgs_data_orig,Label == "b")[parameter]
+  PGivenS <- filter(higgs_data_orig,Label == "s")[parameter]
+  PB <- discretize(PGivenB, nbins = 200)
+  print(PB)
+  PS <- discretize(PGivenS,nbins = 200)
+  print(PS)
+  return(mutinformation(PB,PS))
+}
+
+
 header <- names(higgs_vars)
 MIData <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
+Mutual_Information2(header[1])
+sapply(header,Mutual_Information2)
+MIData2 <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information2))))
 MI_Data_Ordered <- MIData[order(MIData$data),]
+MI_Data2_Ordered <- MIData2[order(MIData2$data),]
 head(MI_Data_Ordered,n= 10)
+MI_Data2_Ordered
 
