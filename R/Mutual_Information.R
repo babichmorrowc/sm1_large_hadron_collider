428dfa1c142df72ff8ea63676389bb21ecc79949
NonDescriptMaths <kieranmorris77@gmail.com>
2024-01-11 10:21:49 +0000

    implimenting new mutinfo

--- R/Mutual_Information.R
+++ R/Mutual_Information.R
@@ -4,24 +4,35 @@ library(infotheo)
 
 source(here("R/clean_data.R"))
 
-#|-----------------------------------------------|
-
-
-
+#|Mutual Info-----------------------------------------|
 
 Mutual_Information<- function(parameter){
   Variable = higgs_data_orig[parameter]
   Labels = higgs_data_orig["Label"]
+  #take variable data form higgs orig
   Labels[Labels == "s"] <- "1"
   Labels[Labels == "b"] <- "0"
   Labels_Val <- apply(Labels,2,as.numeric)
+  #replace with numeric for mutinfo
   ParaB <- discretize(Variable, nbins = 20000)
   ParaS <- discretize(Labels_Val,nbins = 20000)
+  #mutinformation requires binned data
   return(mutinformation(ParaB,ParaS,method = "emp"))
 }
+#|----------------------------------------------------|
 
-#COMPUTE MUTUAL INFO BETWEEN VAR and Y
-MIData <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
-MI_Data_Ordered <- MIData[order(MIData$data),]
+#|Variables to Scan-----------------------------------|
+header = names(higgs_data_orig[2:30])
+#|----------------------------------------------------|
 
+#Compute MI-------------------------------------------|
+#The code below finds MIdata, we have a local copy as it is computationally long
 
+#MIData <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
+#print(MIData)
+#MI_Data_Ordered <- MIData[order(MIData$data),]
+#|----------------------------------------------------|
+
+#Local Copy-------------------------------------------|
+MI_Data_STORED <- data.frame(names = header,data=c(0.157847348,0.099242021,0.090955700,0.034024850,0.033387741,0.032628663,0.032134847,0.018698667,0.010926408,0.039696267,0.043557790,0.048218121,0.025173021,0.064883381,0.009364819,0.004573067,0.013445495,0.013914809,0.003812932,0.027523670,0.003771850,0.031078017,0.019823370,0.026665620,0.025529809,0.015581755,0.013785587,0.020635994,0.013326433))
+MI_Data_STORED_Ordered <- MI_Data_STORED[order(MI_Data_STORED$data,decreasing = TRUE),]
