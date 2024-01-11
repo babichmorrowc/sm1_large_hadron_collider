2dfed3781f1f4567d5e8084cf8a803abcb58f4e1
NonDescriptMaths <kieranmorris77@gmail.com>
2024-01-11 09:45:39 +0000

    Update Mutual_Information.R

--- R/Mutual_Information.R
+++ R/Mutual_Information.R
@@ -6,21 +6,22 @@ source(here("R/clean_data.R"))
 
 #|-----------------------------------------------|
 
+
+
+
 Mutual_Information<- function(parameter){
-  PGivenB <- filter(higgs_data_orig,Label == "b")[parameter]
-  PGivenS <- filter(higgs_data_orig,Label == "s")[parameter]
-  PGivenB_Est <- sample(as.vector(unlist(PGivenB)),size = 20000,replace = FALSE)
-  PGivenS_Est <- sample(as.vector(unlist(PGivenS)),size = 20000,replace = FALSE)
-  ParaB <- discretize(PGivenB_Est, nbins = 20000)
-  ParaS <- discretize(PGivenS_Est,nbins = 20000)
+  Variable = higgs_data_orig[parameter]
+  Labels = higgs_data_orig["Label"]
+  Labels[Labels == "s"] <- "1"
+  Labels[Labels == "b"] <- "0"
+  Labels_Val <- apply(Labels,2,as.numeric)
+  ParaB <- discretize(Variable, nbins = 20000)
+  ParaS <- discretize(Labels_Val,nbins = 20000)
   return(mutinformation(ParaB,ParaS,method = "emp"))
 }
 
+#COMPUTE MUTUAL INFO BETWEEN VAR and Y
+MIData <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
+MI_Data_Ordered <- MIData[order(MIData$data),]
 
-
-header <- names(higgs_vars[0:30])
-MIData2 <- data.frame(names = header, data = as.vector(unlist(sapply(header,Mutual_Information))))
-MI_Data2_Ordered <- MIData2[order(MIData2$data),]
-head(MI_Data2_Ordered,n= 10) #these are the lowest mutual information scoring variables
-MI_Data2_Ordered 
 
